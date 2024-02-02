0d09fcf88d49660669334ee96e5a3754cdf5aa48
Ronaldo Arley <ronaldmamani140@gmail.com>
2024-02-02 09:59:13 -0300

    Adicionando a seção Faqs na pagina about

--- src/app/components/faqs/faqs.component.ts
+++ src/app/components/faqs/faqs.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./faqs.component.scss']
 })
 export class FAQsComponent {
+  readMeWorks1 : boolean = false
+  readMeWorks2 : boolean = false
+  readMeWorks3 : boolean = false
 
+  readMeDriving1 : boolean = false
+  readMeDriving2 : boolean = false
+  readMeDriving3 : boolean = false
 }
--- src/app/components/read-more/read-more.component.ts
+++ src/app/components/read-more/read-more.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-read-more',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./read-more.component.scss']
 })
 export class ReadMoreComponent {
-
+  @Input() open : boolean = false
+  @Input() TitleReadMe : string = ''
+  @Input() TextDown : string = ''
 }
